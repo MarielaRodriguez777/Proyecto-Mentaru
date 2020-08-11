d2f738403ad47afa70ddfdc4b976ca66c73f984f
Luis <luisfer.sm15@gmail.com>
2020-08-11 12:16:16 -0600

    Estructura de carpetas

---
+++ BD/Readme-Estructura-de-carpetas.txt
@@ -0,0 +1,17 @@
+Descripcion y estructura de Carpetas
+1. DDLs: En esta carpeta estaran todos los scripts DDL necesarios para crear la Base de Datos
+    
+2. DMLs: En esta carpetan estaran todos los scripts DML necesarios para llenar la Base de Datos
+
+3. Funciones: En esta carpetan estaran todas las funciones de utilidad que seran usadas por los procedimientios Almacenados
+    - Se identifica con el prefijo: FN_
+
+4. Modelo Relacional: En esta carpetan estaran todos los archivos que describen el Modelo Relacional de la Base de Datos
+
+5. Procedimientos: En esta carpeta estaran todos los procedimientos almacenados respectivos para cada modulo
+    - Se identifica con el prefijo: PR_
+    
+6. Vistas: En esta carpeta estaran todas las vistaas que seran usadas por los procedimientos Almacenados
+    - Se identifica con el prefijo: VI_
+
+    
---
+++ Mentaru/js/script-estadisticas.js
@@ -0,0 +1,77 @@
+am4core.ready(function() {
+    // Themes begin
+    am4core.useTheme(am4themes_animated);
+    // Themes end
+    
+    // **********************************
+    // ***** ESTADISTICAS GENERALES *****
+    // 1. Cifras generales sobre casos confirmados
+    var generalCifrasConfirmadas = am4core.create("estadisticas-generales-cifras-nacionales", am4charts.XYChart);
+    generalCifrasConfirmadas.data = [{
+      "estado": "Confirmados",
+      "casos": 47454
+    }, {
+      "estado": "Recuperados",
+      "casos": 6597
+    }, {
+      "estado": "Muertes",
+      "casos": 1495
+    }];
+    var categoria_GeneralCifrasConfirmadas = generalCifrasConfirmadas.xAxes.push(new am4charts.CategoryAxis());
+    categoria_GeneralCifrasConfirmadas.dataFields.category = "estado";
+    categoria_GeneralCifrasConfirmadas.renderer.grid.template.location = 0;
+    categoria_GeneralCifrasConfirmadas.renderer.minGridDistance = 30;    
+    categoria_GeneralCifrasConfirmadas.renderer.labels.template.adapter.add("dy", function(dy, target) {
+      if (target.dataItem && target.dataItem.index & 2 == 2) {
+        return dy + 25;
+      }
+      return dy;
+    });    
+    var valueAxis_GeneralCifrasConfirmadas = generalCifrasConfirmadas.yAxes.push(new am4charts.ValueAxis());
+    var series = generalCifrasConfirmadas.series.push(new am4charts.ColumnSeries());
+    series.dataFields.valueY = "casos";
+    series.dataFields.categoryX = "estado";
+    series.name = "casos";
+    series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
+    series.columns.template.fillOpacity = .8;
+    var plantillaColumna_GeneralCifrasConfirmadas = series.columns.template;
+    plantillaColumna_GeneralCifrasConfirmadas.strokeWidth = 2;
+    plantillaColumna_GeneralCifrasConfirmadas.strokeOpacity = 1;
+    
+    // **********************************
+    // ****** ESTADISTICAS LOCALES ******
+    // 1. Sintomas comunes
+    var localSintomasComunes = am4core.create("estadisticas-locales-sintomas-comunes", am4charts.XYChart);
+    localSintomasComunes.data = [{
+      "sintomaComun": "Tos",
+      "personas": 2
+    }, {
+      "sintomaComun": "Fiebre",
+      "personas": 2
+    }, {
+      "sintomaComun": "Diarrea",
+      "personas": 2
+    }];
+    var categoria_LocalSintomasComunes = localSintomasComunes.xAxes.push(new am4charts.CategoryAxis());
+    categoria_LocalSintomasComunes.dataFields.category = "sintomaComun";
+    categoria_LocalSintomasComunes.renderer.grid.template.location = 0;
+    categoria_LocalSintomasComunes.renderer.minGridDistance = 30;    
+    categoria_LocalSintomasComunes.renderer.labels.template.adapter.add("dy", function(dy, target) {
+      if (target.dataItem && target.dataItem.index & 2 == 2) {
+        return dy + 25;
+      }
+      return dy;
+    });    
+    var valueAxis_LocalSintomasComunes = localSintomasComunes.yAxes.push(new am4charts.ValueAxis());
+    series = localSintomasComunes.series.push(new am4charts.ColumnSeries()); // * Probar reutilizar variables
+    series.dataFields.valueY = "personas";
+    series.dataFields.categoryX = "sintomaComun";
+    series.name = "personas";
+    series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
+    series.columns.template.fillOpacity = .8;
+    var plantillaColumna_LocalSintomasComunes = series.columns.template;
+    plantillaColumna_LocalSintomasComunes.strokeWidth = 2;
+    plantillaColumna_LocalSintomasComunes.strokeOpacity = 1;
+
+    
+    }); // end am4core.ready()
