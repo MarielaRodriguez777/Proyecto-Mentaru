11a42b0740e7ef45467d0eb367629a280415dbe9
oscarfonse <43530787+oscarfonse@users.noreply.github.com>
2020-08-16 23:40:31 -0600

    scripts

---
+++ Mentaru/js/scripts/Encuesta/peticiones.js
@@ -0,0 +1,53 @@
+// Aqui estaran todas las peticiones AJAX
+const FN_Peticion_IdentificarPersona = () => {
+	// Identificar la persona con las pruebas del COVID-19
+	// Realizar peticion AJAX
+
+	/** En caso de identificar al usuario, hacer:
+	 * Actualizar data_persona
+	 * 
+	 * Llamar a las funciones:
+	 * FN_Peticion_Departamentos
+	 * FN_Peticion_Antecedents
+	 * FN_Peticion_Sintomas
+	 * FN_Peticion_AsistenciaMedica
+	 * 
+	 * LLamar las funciones de renderizar:
+	 * FN_Render_InfoPersona
+	 * FN_Render_Departamentos
+	 * FN_Render_Patologias
+	 * FN_Render_NoPatologias
+	*/
+   
+	imprimir('Se ha identificado la persona');
+}
+
+const FN_Peticion_Departamentos = () => {
+	// Realizar peticion AJAX
+	// Actualizar data_departamentos
+	imprimir('Se ha actualizado la data de departamentos');
+}
+
+const FN_Peticion_Antecedentes = () => {
+	// Realizar peticion AJAX
+	// Actualizar data_antecedentes
+	imprimir('Se ha actualizado la data de antecedentes');
+}
+
+const FN_Peticion_Sintomas = () => {
+	// Realizar peticion AJAX
+	// Actualizar data_sintomas
+	imprimir('Se ha actualizado la data de sintomas');
+}
+
+const FN_Peticion_AsistenciaMedica = () => {
+	// Realizar peticion AJAX
+	// Actualizar data_asistencia_medica
+	imprimir('Se ha actualizado la data de asistencia medica');
+}
+
+const FN_Peticion_EnviarEncuesta = () => {
+	// Realizar peticion AJAX
+	// Enviar todos los datos recopilados por la encuesta
+	imprimir('Se ha enviado la encuesta al servidor');
+}
---
+++ Mentaru/js/scripts/Encuesta/render.js
@@ -0,0 +1,40 @@
+// Aqui estaran todas las renderizaciones de la pantalla encuesta
+const FN_Render_InfoPersona = () => {
+	// Actualizar la informacion del paciente
+	imprimir('Se ha actualizado la informacion del paciente');
+}
+
+const FN_Render_Departamentos = () => {
+	// Actualizar combobox de Departamentos
+	imprimir('Se ha actualizado el combobox de Departamentos');
+}
+
+const FN_Render_Municipios = () => {
+	// Actualizar combobox de Municipio
+	imprimir('Se ha actualizado el combobox de Municipios');
+}
+
+const FN_Render_Patologias = () => {
+	// Actualizar lista de Patologias
+	imprimir('Se ha actualizado la lista de Patologias');
+}
+
+const FN_Render_NoPatologias = () => {
+	// Actualizar lista de No Patologias
+	imprimir('Se ha actualizado la lista de No Patologias');
+}
+
+const FN_Render_Sintomas = () => {
+	// Actualizar lista de Sintomas
+	imprimir('Se ha actualizado la lista de Sintomas');
+}
+
+const FN_Render_Tratamientos = () => {
+	// Actualizar combobox de Tratamientos
+	imprimir('Se ha actualizado el combobox de Tratamientos');
+}
+
+const FN_Render_LugarAsistencia = () => {
+	// Actualizar combobox de Lugar de Asistencia
+	imprimir('Se ha actualizado el combobox de Lugar de Asistencia');
+}
