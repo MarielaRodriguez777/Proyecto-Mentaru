bbe1f93c23a3c9caabc7f8fc5c14a0fdbfc47544
Luis <luisfer.sm15@gmail.com>
2020-08-18 16:16:09 -0600

    BD: Creacion del archivo DML_Delete.sql

---
+++ BD/DMLs/DML_Delete.sql
@@ -0,0 +1,30 @@
+-- -----------------------------------------------------
+-- Schema MENTARUDB
+-- -----------------------------------------------------
+USE MENTARUDB;
+
+
+DELETE FROM ListaAntecedentesNP;
+DELETE FROM ListaAntecedentesP;
+DELETE FROM AntecedentesP;
+DELETE FROM Usuarios;
+DELETE FROM CronologiaContagios;
+DELETE FROM AntecedentesNP;
+DELETE FROM ListaPruebas;
+DELETE FROM Prueba;
+DELETE FROM AsistenciaMedica;
+DELETE FROM LugarAsistencia;
+DELETE FROM Tratamiento;
+DELETE FROM Laboratorio;
+DELETE FROM listaSintomas;
+DELETE FROM Sintoma;
+DELETE FROM TipoSintoma;
+DELETE FROM Cronologia;
+DELETE FROM Encuesta;
+DELETE FROM EstadoPaciente;
+DELETE FROM NoPatologia;
+DELETE FROM Patologia;
+DELETE FROM Persona;
+DELETE FROM Municipio;
+DELETE FROM Departamento;
+DELETE FROM Genero;
