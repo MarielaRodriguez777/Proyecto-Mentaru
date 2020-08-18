d1434050f2266096fb1f46d43f98c65d3924a0b3
Luis <luisfer.sm15@gmail.com>
2020-08-17 18:32:02 -0600

    SERVER: Probando APIS

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
 const config = require('./Server/settings/config');
+
+// IMPORTANTDO APIS
 const apisViews = require('./Server/views/apisViews');
+const apisEN = require('./Server/apis/apiEN');
+//const apisES = require('./Server/apis/apiES');
+//const apisUS = require('./Server/apis/apiUS');
+//const apisMiscelaneos = require('./Server/apis/apiMiscelaneos');
 
 // DEFINIENDO LA VARIABLE 'app' QUE ES EL SERVIDOR
 const app = express();
@@ -14,6 +20,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(session(config.configSession));
 app.use('/mentaru/', apisViews);
+app.use('/mentaru/api/EN', apisEN);
+//app.use('/mentaru/api/ES', apisES);
+//app.use('/mentaru/api/US', apisUS);
+//app.use('/mentaru/api/Miscelaneos', apisMiscelaneos);
+
+
+// Punto de inicio
+app.get('/mentaru/probarAPI', (req, res) => {
+    /*if (req.session.name) {
+    	functionsMiscelaneos.redirectById(req, res);
+    } else {
+    	res.redirect('/Mentaru/view/index');
+    }*/
+    res.redirect('/mentaru/api/EN/EN_IDENTIFICAR');
+});
 
 // Punto de inicio
 app.get('/mentaru/', (req, res) => {
